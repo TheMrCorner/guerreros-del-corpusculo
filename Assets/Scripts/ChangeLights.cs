4e2adbe4933fb92fec61bcc59aac329c510cb0f3
EvSanz <evsanc07@ucm.es>
2022-01-29 20:27:07 +0100

    Paneles de control para activar o desactivar luces

--- Assets/Scripts/ChangeLights.cs
+++ Assets/Scripts/ChangeLights.cs
@@ -8,15 +8,19 @@ public class ChangeLights : MonoBehaviour
     public Light lightSol;
     public Light[] colorLights;
 
+    bool darLuz; 
+
     void Start()
     {
         lightSol.enabled = true;
 
         for (int i = 0; i < colorLights.Length; i++)
-            colorLights[i].enabled = false; 
+            colorLights[i].enabled = false;
+
+        darLuz = false; 
     }
 
-    void Update()
+    void OnTriggerStay(Collider other)
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
