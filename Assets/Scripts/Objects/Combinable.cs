96c350615deba53a248aa33781b500dd8ce1442f
Amphxros <amprubio@ucm.es>
2022-01-29 17:49:10 +0100

    Cabecero para el inspector

--- Assets/Scripts/Objects/Combinable.cs
+++ Assets/Scripts/Objects/Combinable.cs
@@ -2,8 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+// Sitio donde usar un objeto
 public class Combinable : MonoBehaviour
 {
+
+    [Header ("Nombre del objeto necesario")]
     public string requested_object;
 
     private void Start() {
