3967ee892e1701a8293faea16aada96e9d2871e9
Alejandro Marín <57327264+TheMrCorner@users.noreply.github.com>
2022-01-29 21:43:22 +0100

    Inventory + FPS

--- Assets/Scripts/FPS/CameraLook.cs
+++ Assets/Scripts/FPS/CameraLook.cs
@@ -14,6 +14,7 @@ namespace FPS
         #region Private
         float _xRotation = 0f;
         float _mouseX, _mouseY;
+        bool _isInventory = false;
         #endregion
         #endregion
 
@@ -21,22 +22,40 @@ namespace FPS
         // Start is called before the first frame update
         void Start()
         {
-            Cursor.visible = false;
-            Cursor.lockState = CursorLockMode.Locked;
+            ManageInventory(false);
         } // Start
 
         // Update is called once per frame
         void Update()
         {
-            _mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-            _mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+            if (!_isInventory)
+            {
+                _mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+                _mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
-            _xRotation -= _mouseY;
-            _xRotation = Mathf.Clamp(_xRotation, -90f, 90f);
+                _xRotation -= _mouseY;
+                _xRotation = Mathf.Clamp(_xRotation, -90f, 90f);
 
-            transform.localRotation = Quaternion.Euler(_xRotation, 0f, 0f);
-            playerBody.Rotate(Vector3.up * _mouseX);
+                transform.localRotation = Quaternion.Euler(_xRotation, 0f, 0f);
+                playerBody.Rotate(Vector3.up * _mouseX);
+            } // if
         } // Update
+
+        public void ManageInventory(bool status)
+        {
+            if (status)
+            {
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.None;
+                _isInventory = true;
+            } // if
+            else
+            {
+                Cursor.visible = false;
+                Cursor.lockState = CursorLockMode.Locked;
+                _isInventory = false;
+            }
+        } // ManageInventory
         #endregion
     } // CameraLook
 } // namespace
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -2,16 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Events;
 public class UIManager : MonoBehaviour
 {
+    public class UIStatus : UnityEvent<bool>
+    {
+    }
+
     public Text inventoryList;
     public GameObject inventory;
+    public UIStatus statusNotif;
 
 
     public void init(){
         inventoryList.text="";
 
         desactivateUI();
+    }
+
+    private void Awake()
+    {
+        statusNotif = new UIStatus();
     }
 
     void Start()
@@ -21,13 +32,14 @@ public class UIManager : MonoBehaviour
 
     public void activateUI(){
         inventory.SetActive(true);
+        statusNotif.Invoke(true);
         updateInventoryList();
 
     }
 
     public void desactivateUI(){
         inventory.SetActive(false);
-
+        statusNotif.Invoke(false);
     }
 
     public void updateInventoryList(){
