4be1f21fdd670866084530d8907176538fb3a14d
EvSanz <evsanc07@ucm.es>
2022-01-30 00:53:42 +0100

    CameraLook

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraLook : MonoBehaviour
+public class MouseLook : MonoBehaviour
 {
     #region Variables
     #region Public
