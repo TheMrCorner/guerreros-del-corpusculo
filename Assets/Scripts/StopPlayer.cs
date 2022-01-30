968992fd2a15b31775ff2ddcfaca484bb9bd00d3
EvSanz <evsanc07@ucm.es>
2022-01-30 01:58:25 +0100

    movimiento pieza uno

--- Assets/Scripts/StopPlayer.cs
+++ Assets/Scripts/StopPlayer.cs
@@ -10,6 +10,7 @@ public class StopPlayer : MonoBehaviour
     void OnTriggerEnter(Collider other)
     {
         player = other.GetComponent<PlayerMovement>();
+        mouse = other.GetComponentInChildren<MouseLook>();
     }
 
     void OnTriggerStay(Collider other)
@@ -18,6 +19,7 @@ public class StopPlayer : MonoBehaviour
         if (player != null && Input.GetKeyUp(KeyCode.E))
         {
             player.enabled = !player.enabled;
+            mouse.enabled = !mouse.enabled; 
 
             if (!player.enabled)
                 Cursor.lockState = CursorLockMode.Confined;
